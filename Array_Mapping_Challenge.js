c6e8ccb1a3d306c4901c9d34151ac96077bba9f0
Rohit <moza.rohit@gmail.com>
2017-04-06 16:18:30 +0000

    Used map to go through the array

--- Array_Mapping_Challenge.js
+++ Array_Mapping_Challenge.js
@@ -1,9 +1,16 @@
+
+
+// Data to work with
 var input = [
   { x: 3, y: 4 },
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
-var result = input.map(/* Your code here */);
+
+// Here we start our function
+var result = input.map(function(element){
+  return Math.sqrt(element.x*element.x + element.y*element.y);
+});
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
